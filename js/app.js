64d0be69a011250387720a7f70cb3a93585d703c
inga Bacher <ingalila@hotmail.fr>
2019-06-12 19:36:19 +0200

    ajout

---
+++ js/app.js
@@ -0,0 +1,143 @@
+import stars from "./stars.js";
+// console.log(stars.length);
+
+const pointsRule = {
+  base: 40,
+  1: 10,
+  2: 20,
+  3: 30,
+  0: 0
+};
+
+var round = {
+  currentImageIndex: null,
+  level: 1,
+  maskClicks: 0
+};
+
+var playerPoints = 0;
+
+const btnStart = document.getElementById("starter_btn");
+const gameBoard = document.getElementById("game_board");
+const textResponse = document.getElementById("text_response");
+const btnResponse = document.getElementById("btn_response");
+const showRound = document.getElementById("show_round");
+const showScore = document.getElementById("show_score");
+const answerInput = document.getElementById("text_response");
+const starPict = document.getElementById("current_star");
+const masks = document.getElementsByClassName("mask-item");
+var currentStar = null;
+
+function verifyUserResponse() {
+  console.log("current star ?", currentStar.name);
+  if (
+    currentStar.name.toLowerCase().replace(" ", "") ===
+    answerInput.value.toLowerCase().replace(" ", "")
+  ) {
+    // console.log("bonne réponse !!!");
+    // console.log(round);
+    // console.log(pointsRule[round.maskClicks]);
+    playerPoints += pointsRule.base - pointsRule[round.maskClicks];
+  } else {
+    console.log("mauvaise réponse, vous perdez tous vos acquis");
+    playerPoints = 0;
+  }
+
+  // 1 definir un input de type text en HTML (avec un id)
+  // 2 depuis JS, selectionner cet input (par so id)
+  // 3 ajouter un event listener sur cet input
+  // indice = le type d'évent que tu souhaite écouter = change
+  // 4 associer une function (handler)
+  // 5 afficher un log coucou dans la console à chaque changement
+}
+
+function disappearMask() {
+  document.getElementsByClassName(".mask").forEach(function($mask) {
+    if (mask.style.backgroundColor === " green") {
+      $mask.style.visibility = "visible";
+    } else {
+      $mask.style.visibility = "hidden";
+
+      mask.onclick = function() {
+        displayImage();
+      };
+      console.log("bon");
+    }
+  });
+}
+
+function displayImage() {
+  const number = Math.floor(Math.random() * stars.length);
+  currentStar = stars[number];
+  starPict.src = currentStar.path;
+  round.currentImageIndex = number;
+  console.log(currentStar);
+  // 1 = recup l'image avec getElementById
+  // 2 = affecter une variable img avec le result de getElementById
+  // ci-dessus: c'est un OBJECT !!!
+  // tu veux accéder à sa propriété src
+  // tu peux changer cette source (avec le signe = )
+  // donc tu peux associer la src avec le path
+  return currentStar;
+}
+
+function updateDisplay() {
+  showRound.textContent = "Round n°" + round.level;
+  showScore.textContent = "Your score : " + playerPoints;
+}
+
+function goToNextRound() {
+  console.log("on passe au next round");
+  console.log(round, playerPoints);
+  stars.splice(round.currentImageIndex, 1); // check sur mdn la doc
+  round.level++;
+  round.maskClicks = 0;
+  resetMasks();
+  updateDisplay();
+  setTimeout(function() {
+    displayImage();
+  }, 1000);
+}
+
+function resetMasks(evt) {
+  for (let i = 0; i < masks.length - 1; i++) {
+    console.log(i);
+    masks[i].style.background = "goldenrod";
+    masks[i].style.borderColor = "black";
+  }
+}
+
+function initMasks() {
+  function revealMask(evt) {
+    evt.target.style.background = "transparent";
+    evt.target.style.borderColor = "transparent";
+    round.maskClicks++;
+  }
+
+  for (let i = 0; i < masks.length - 1; i++) {
+    masks[i].onclick = revealMask;
+  }
+}
+
+function startGame() {
+  btnStart.classList.add("is-hidden");
+  gameBoard.classList.remove("is-hidden");
+  showRound.classList.remove("is-hidden");
+  showScore.classList.remove("is-hidden");
+  textResponse.classList.remove("is-hidden");
+  btnResponse.classList.remove("is-hidden");
+  updateDisplay();
+  displayImage();
+  initMasks();
+}
+
+// EVENT LISTENERS !!! on écoute les évènements sur les éléments HTML
+btnStart.onclick = startGame;
+btnResponse.onclick = function() {
+  verifyUserResponse();
+  goToNextRound();
+};
+// créer 4 carrés de dimension égal dont un invisible
+// les superposer sur la photo 800x800
+// que les carrés de différentes couleurs se déplacent aléatoirement à chaque changement de photo
+// pouvoir cliquer sur le carré choisi pour voir davantage l'image apparaitre, retire des points plus tu retires de carrés
---
+++ js/stars.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: "Donald Trump",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560178976/GUESSWHO/donaldtrump_pszhaf.jpg"
+  },
+  {
+    name: "Mickey Mouse",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560179280/GUESSWHO/mickeymouse_s7r7s2.jpg"
+  },
+  {
+    name: "Rihanna",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560178830/GUESSWHO/rihanna_ppa7ja.jpg"
+  },
+  {
+    name: "Lady Gaga",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560178698/GUESSWHO/ladygaga-01_htshev.jpg"
+  },
+  {
+    name: "Eddie Murphy",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560179256/GUESSWHO/eddiemurphy_vho1a1.jpg"
+  },
+  {
+    name: "Jean Reno",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560341589/GUESSWHO/jeanreno_uujgjw.jpg"
+  },
+  {
+    name: "Johnny Hallyday",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560335641/GUESSWHO/johnny_dk67ij.jpg"
+  },
+  {
+    name: "Edith Piaf",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560335668/GUESSWHO/piaf_edith_kq0t61.jpg"
+  },
+  {
+    name: "Michael Jackson",
+    path:
+      "https://res.cloudinary.com/inga-bacher/image/upload/v1560335650/GUESSWHO/michael-jackson_hgacow.jpg"
+  }
+];
